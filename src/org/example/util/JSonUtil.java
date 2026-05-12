6c2642afe71465c787629bc90419f146949419e7
25023526 <25023526@vnu.edu.vn>
2026-05-12 08:49:29 +0700

    Add server files

--- src/org/example/util/JSonUtil.java
+++ src/org/example/util/JSonUtil.java
@@ -3,7 +3,7 @@ package org.example.util;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class JsonUtil {
+public class JSonUtil {
 
     public static String toJson(Object obj) {
         if (obj instanceof Map<?, ?> map) {
